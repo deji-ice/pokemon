272aa2ac0785ff1b18f22566d39115641b9ecdfe
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-06-12 15:33:17 +0100

    feat: Improve Pokemon image display functionality

--- pokemon.js
+++ pokemon.js
@@ -1,29 +1,34 @@
-const form = document.getElementById("form"); 
-const fetchPokemon = async () => {
-  const pokemonName = document.getElementById("input").value;
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
-    method: "GET",
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => res.sprites.front_default)
-    .catch((err) => console.log("err", err));
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  const fetchPokemon = async () => {
+    const pokemonName = document.getElementById("input").value;
+    const data = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+      {
+        method: "GET",
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => res.sprites.front_default)
+      .catch((err) => console.log("err", err));
     const img = document.createElement("img");
-    img.src = data
+    img.src = data;
     document.body.appendChild(img);
-};
+  };
 
-form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-clearUI()
+    clearUI();
     fetchPokemon();
-});
+  });
 
-const clearUI = () => {
-  const img = document.querySelector("img");
-  if (img) {
-    img.remove();
-  }
-};
+  const clearUI = () => {
+    const img = document.querySelector("img");
+    if (img) {
+      img.remove();
+    }
+  };
+});
