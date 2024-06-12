cc9bfe25b2c445264bd2df501a5d9bf6d29a15b8
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-06-12 13:34:22 +0100

    feat: Add Pokemon image display functionality
    The code changes introduce the ability to fetch and display the image of a Pokemon based on user input. The `fetchPokemon` function retrieves the image URL from the PokeAPI and creates an `img` element to display it. The `clearUI` function removes any existing image before fetching a new one.

---
+++ pokemon.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form"); 
+const fetchPokemon = async () => {
+  const pokemonName = document.getElementById("input").value;
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => res.sprites.front_default)
+    .catch((err) => console.log("err", err));
+    const img = document.createElement("img");
+    img.src = data
+    document.body.appendChild(img);
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+clearUI()
+    fetchPokemon();
+});
+
+const clearUI = () => {
+  const img = document.querySelector("img");
+  if (img) {
+    img.remove();
+  }
+};
